3578498e5c4b5e3a05bffc4d44027502fd39c3f4
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-04-15 22:14:29 +0100

    🎉 Initial commit

---
+++ config/akairo.ts
@@ -0,0 +1,5 @@
+import { AkairoOptions } from "discord-akairo";
+
+export const akairo: AkairoOptions = {
+    ownerID: "217562587938816000",
+};
---
+++ lib/bugbot.ts
@@ -0,0 +1,14 @@
+import { AkairoClient } from "discord-akairo";
+
+class BugBot extends AkairoClient {
+    constructor() {
+        super({
+            // Options for Akairo go here.
+        }, {
+            // Options for discord.js goes here.
+        });
+    }
+}
+
+const client = new MyClient();
+client.login('TOKEN');
