0ce64504bd506439562eaa3bbb3ff6bdbfee2bde
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-04-15 22:20:58 +0100

    ✨ Add dotenv

--- config/discord.ts
+++ config/discord.ts
@@ -1,5 +1,9 @@
 import { DiscordConfig } from "./types";
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const discordConfig: DiscordConfig = {
     token: process.env.TOKEN // change this in .env
 }
