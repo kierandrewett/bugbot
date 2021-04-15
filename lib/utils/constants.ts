e68947a48022ebabd01eb6ff516afcfdf5d148c5
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-04-15 23:49:47 +0100

    😁 Add status emotes

---
+++ lib/utils/constants.ts
@@ -0,0 +1,4 @@
+export const emotes = {
+    success: "<:bug_check:832386339328622603>",
+    error: "<:bug_fail:832386242783215756>"
+}
--- src/commands/report.ts
+++ src/commands/report.ts
@@ -1,14 +1,25 @@
+import { emotes } from "../../lib/utils/constants";
 import { Command } from "discord-akairo";
+import { DiscordAPIError } from "discord.js";
 import { Message } from "discord.js";
 
 export default class ReportCommand extends Command {
     public constructor() {
         super('report', {
-            aliases: ['report']
+            aliases: ['report'],
         });
     }
 
     public exec(message: Message) {
-        return message.reply(`🚧`, { replyTo: message })
+        message.author.send("hi")
+            .then(_ => {
+                message.react("📬")
+            })
+            .catch((e: DiscordAPIError) => {
+                switch(e.code) {
+                    case(50007):
+                        message.reply(`${emotes.error} We were unable to direct message you!`, { replyTo: message })
+                }
+            })
     }
 }
