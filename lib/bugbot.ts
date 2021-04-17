6612060b44e6ee341df19ce024cf648a02847f8f
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-04-17 16:18:45 +0100

    ✨ Add db

--- lib/bugbot.ts
+++ lib/bugbot.ts
@@ -1,5 +1,6 @@
 import { AkairoClient, CommandHandler, InhibitorHandler, ListenerHandler, SQLiteProvider } from "discord-akairo";
-
+import { open } from "sqlite";
+import { Database } from "sqlite3";
 import { config, akairoConfig, discordConfig } from "../config";
 
 class BugBot extends AkairoClient {
@@ -28,7 +29,15 @@ class BugBot extends AkairoClient {
             directory: './src/events'
         });
 
-        this.settings = new SQLiteProvider(open("./db.sqlite"), 'data');
+        this.settings = new SQLiteProvider(open({
+            filename: "db.sqlite",
+            driver: Database
+        }), 'data', {
+            idColumn: 'id',
+            dataColumn: 'data'
+        });
+
+        // this.initDB();
 
         this.commandHandler.useInhibitorHandler(this.inhibitorHandler);
         this.commandHandler.useListenerHandler(this.listenerHandler);
@@ -40,6 +49,10 @@ class BugBot extends AkairoClient {
         process.on("uncaughtException", (e) => {
             console.log(e)
         })
+    }
+
+    public async initDB() {
+        await this.settings.init()
     }
 }
 
